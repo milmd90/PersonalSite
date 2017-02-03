a2000f906031df1a93647c965360b310f498f99e
Matthew Miller <mattdeanmiller@gmail.com>
2017-02-02 21:52:53 -0600

    Restructuring

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Router, Route, Link, browserHistory } from 'react-router';
 
 var Base = require('./Base.jsx');
 var Home = require('./Home.jsx');
+var Blog = require('./Blog.jsx');
 var ErrorPage = require('./ErrorPage.jsx');
 
 class App extends Component {
@@ -10,9 +11,9 @@ class App extends Component {
       return (
           <div>
               <Router history={browserHistory}>
-                  {/* <Route path="/" component={Base}/> */}
                   <Route path="/" component={Base}>
                       <Route path="/home" component={Home}/>
+                      <Route path="/blog" component={Blog}/>
                   </Route>
                   <Route path="*" component={ErrorPage}/>
               </Router>
