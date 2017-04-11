150bdb7b6fd4c9997b4fb3716df80f9bd5620a12
Matthew Miller <mattdeanmiller@gmail.com>
2017-04-11 12:30:59 -0500

    Correct home github link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ class Home extends Component {
                              I am a frontend web developer working at OrderMyGear in Dallas. <br/>
                              I also enjoy playing guitar and eating sandwiches. <br/>
                              This website is a work in progress. Feel free to view the source code on
-                             <a class="hover-bold" href="https://github.com/milmd90/PersonalSite">GitHub</a>
+                             <a className="hover-bold" href="https://github.com/milmd90/PersonalSite"> GitHub</a>.
                          </p>
                      </div>
                  </div>
