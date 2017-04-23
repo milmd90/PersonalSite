45e880cd5139a623f049af973f22c3ca949eea2d
Matthew Miller <mattdeanmiller@gmail.com>
2017-04-23 10:45:38 -0500

    Formatting personal posts

--- src/posts/Design.jsx
+++ src/posts/Design.jsx
@@ -7,12 +7,19 @@ class Design extends Component {
             <div className={this.props.theme +' post'}>
                 <PostHeader title="Design"
                             link={this.props.link}
-                            created="10 April 2017"
+                            created="23 April 2017"
                 />
                 <div className="post-body">
-                    <img src="/img/logo/logo_black.png" alt="matt"/>
-                    <img src="/img/logo/tanner.png" alt="tanner"/>
-                    <img src="/img/logo/rikeen.png" alt="rikeen"/>
+                    <div className="ib ib-6">
+                        <p>
+                            I've started learning graphic design by making a personal logos for my friends.
+                        </p>
+                    </div>
+                    <div className="ib ib-6">
+                        <img src="/img/logo/logo_black.png" alt="matt"/>
+                        <img src="/img/logo/tanner.png" alt="tanner"/>
+                        <img src="/img/logo/rikeen.png" alt="rikeen"/>
+                    </div>
                 </div>
             </div>
         )
--- src/posts/UncleUbers.jsx
+++ src/posts/UncleUbers.jsx
@@ -5,13 +5,22 @@ class UncleUbers extends Component {
     render() {
         return (
             <div className={this.props.theme +' post'}>
-                <PostHeader title="Uncle Ubers"
+                <PostHeader title="Uncle Uber's"
                             link={this.props.link}
-                            created="10 April 2017"
+                            created="24 April 2017"
                 />
                 <div className="post-body">
-                    <img src="/img/uncleuber/cuban.jpg" alt="cuban"/>
-                    <img src="/img/uncleuber/crispychicken.jpg" alt="cuban"/>
+                    <div className="ib ib-4">
+                        <p>
+                            Uncle Uber's in Deep Ellum has great sandwhiches. I ate both of these.
+                        </p>
+                    </div>
+                    <div className="ib ib-4">
+                        <img src="/img/uncleuber/cuban.jpg" alt="cuban"/>
+                    </div>
+                    <div className="ib ib-4">
+                        <img src="/img/uncleuber/crispychicken.jpg" alt="crispychicken"/>
+                    </div>
                 </div>
             </div>
         )
