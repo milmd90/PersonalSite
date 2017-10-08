9fb3e3a227402d38e8af2e6e96999d09077eee8b
Matthew Miller <mattdeanmiller@gmail.com>
2017-10-07 20:10:33 -0500

    Update linkedin url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ class Footer extends Component {
         return (
             <div className="footer">
                 <a className="fa fa-github" href="https://github.com/milmd90" target="_blank"></a>
-                <a className="fa fa-linkedin" href="https://www.linkedin.com/in/matthew-miller-816462a2/" target="_blank"></a>
+                <a className="fa fa-linkedin" href="https://www.linkedin.com/in/milmd90" target="_blank"></a>
                 <a className="fa fa-spotify" href="https://open.spotify.com/user/126637610" target="_blank"></a>
                 <a className="fa fa-envelope" href="mailto:mattdeanmiller@gmail.com" target="_blank"></a>
             </div>
