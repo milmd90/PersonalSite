8a1c85713f840cf9d3b67da23d5c492dc4850f84
Matthew Miller <mattdeanmiller@gmail.com>
2017-11-22 10:37:23 -0600

    added twitter footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ class Footer extends Component {
             <div className="footer">
                 <a className="fa fa-github" href="https://github.com/milmd90" target="_blank"></a>
                 <a className="fa fa-linkedin" href="https://www.linkedin.com/in/milmd90" target="_blank"></a>
+                <a className="fa fa-twitter" href="https://www.twitter.com/milmd90" target="_blank"></a>
                 <a className="fa fa-spotify" href="https://open.spotify.com/user/126637610" target="_blank"></a>
                 <a className="fa fa-envelope" href="mailto:mattdeanmiller@gmail.com" target="_blank"></a>
             </div>
